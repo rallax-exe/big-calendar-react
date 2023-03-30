28c1e767b5deeca73992cf382470e371ff2a03fd
Raul <raul.lara.unam@gmail.com>
2023-03-30 14:56:00 -0600

    Correcion en ENV, final version 1.0.1

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -2,11 +2,14 @@
 
 export const getEnvVariables = () => {
 
-    import.meta.env;
+    //VITE aun no soluciona el problema de importacion de ENV
+    //import.meta.env;
 
     return{
-
-        ...import.meta.env
+        //Importa de manera manual la variable de entorno
+        //Si tenemos varias variables se necesita importar una por una
+        VITE_API_URL: import.meta.env.VITE_API_URL, 
+        //...import.meta.env
 
     }
 
